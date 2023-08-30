b8748feb6a5142d5d5c97e4c817b69d5e2317d1b
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-30 17:00:14 +0530

    data access layer

---
+++ src/main/java/com/example/tutorial/student/StudentConfig.java
@@ -0,0 +1,37 @@
+package com.example.tutorial.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+import static java.time.Month.*;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(
+            StudentRepository repository){
+        return args -> {
+            Student rishav = new Student(
+                    "Rishav",
+                    22,
+                    LocalDate.of(2000, OCTOBER, 17),
+                    "rishav3141@gmail.com"
+            );
+            Student arushee = new Student(
+                    "Arushee",
+                    21,
+                    LocalDate.of(2002, AUGUST, 22),
+                    "arushee2000@gmail.com"
+            );
+            repository.saveAll(
+                    List.of(rishav, arushee)
+            );
+        };
+    }
+}
--- src/main/java/com/example/tutorial/student/StudentService.java
+++ src/main/java/com/example/tutorial/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.tutorial.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -7,15 +8,15 @@ import java.util.List;
 
 @Service
 public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
     public List<Student> getStudents(){
-        return List.of(
-                new Student(
-                        1L,
-                        "Rishav",
-                        22,
-                        LocalDate.of(2000,10,17),
-                        "rishav3141@gmail.com"
-                )
-        );
+        return studentRepository.findAll();
     }
 }
