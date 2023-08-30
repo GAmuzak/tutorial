4b0ab15fcf267c988ff9871924be7697043e650b
Rishav Banerjee <thatamuzak@gmail.com>
2023-08-30 13:47:32 +0530

    Printing object data as json

--- src/main/java/com/example/tutorial/TutorialApplication.java
+++ src/main/java/com/example/tutorial/TutorialApplication.java
@@ -1,10 +1,12 @@
 package com.example.tutorial;
 
+import com.example.tutorial.student.Student;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
@@ -16,8 +18,16 @@ public class TutorialApplication {
 	}
 
 	@GetMapping
-	public List<String> hello(){
-		return List.of("Hello", "World");
+	public List<Student> hello(){
+		return List.of(
+				new Student(
+						1L,
+						"Rishav",
+						22,
+						LocalDate.of(2000,10,17),
+						"rishav3141@gmail.com"
+				)
+		);
 	}
 
 
